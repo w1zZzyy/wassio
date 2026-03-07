3b7a6c8b007e7f2708919a5182218edf5081c07b
Denis <dkilikki22@gmail.com>
2026-03-07 20:17:33 +0300

    coroutine added

---
+++ include/wassio/core/coroutine.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "wassio/core/promise.hpp"
+
+#include <utility>
+
+namespace wassio::core {
+    
+/* 
+coroutine
+
+- AwaitablePolicy 
+    stores coroutine<Promise> continuation if 
+    - update_continuation method [required]
+    - co_await operator [not required]
+
+- PromiseContainerPolicy
+    promise type configuration
+*/
+template<template <typename> typename AwaitablePolicy, typename PromiseContainerPolicy>
+class Coroutine final : public AwaitablePolicy
+ <Promise<Coroutine<AwaitablePolicy, PromiseContainerPolicy>, PromiseContainerPolicy>> {
+public:
+    using promise_type = Promise<Coroutine, PromiseContainerPolicy>;
+    using handler = promise_type::handler;
+    using awaitable = AwaitablePolicy<promise_type>;
+
+    ~Coroutine() noexcept { destroy(); }
+
+    Coroutine(const Coroutine&) = delete;
+    Coroutine& operator=(const Coroutine&) = delete;
+
+    Coroutine(Coroutine&& o) noexcept
+        : awaitable(o.handler_)
+        , handler_(std::exchange(o.handler_, {}))
+    {}
+
+    Coroutine& operator=(Coroutine&& o) noexcept {
+        if (this != &o) {
+            destroy();
+            handler_ = std::exchange(o.handler_, {});
+            awaitable::update_continuation(handler_);
+        }
+        return *this;
+    }
+
+private:
+    explicit Coroutine(handler h) noexcept : 
+        awaitable(h), 
+        handler_(h) 
+    {}
+    void destroy() noexcept { if(handler_) { handler_.destroy(); } }
+
+    friend class Promise<Coroutine, PromiseContainerPolicy>;
+
+    handler handler_;
+};
+
+}
